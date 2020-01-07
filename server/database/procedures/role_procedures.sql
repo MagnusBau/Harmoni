b340e2f4b0732809c82c629c1ef438f1c6a0b407
Cato Bakken <cato.bakken@ntnu.no>
2020-01-07 15:58:12 +0100

    sql procedures and rest stuff

---
+++ server/database/procedures/role_procedures.sql
@@ -0,0 +1,44 @@
+/**
+  returns all staff-types
+ */
+DELIMITER //
+
+CREATE PROCEDURE get_all_roles()
+BEGIN
+    SELECT role_id, type FROM role;
+END //
+
+DELIMITER ;
+/**
+  Returns staff currently assigned to event
+ */
+DELIMITER //
+
+CREATE PROCEDURE get_staff_in_event(IN event_id_in INT)
+BEGIN
+    SELECT role_id, type, event FROM role WHERE event=event_id_in;
+END //
+
+DELIMITER ;
+/**
+  Sets staff role
+ */
+DELIMITER //
+
+CREATE PROCEDURE set_role(IN role_id_in INT, IN type_in VARCHAR(50), IN event_id_in INT)
+BEGIN
+    INSERT INTO role(role_id, type, event) VALUES (?, ?, ?);
+END //
+
+DELIMITER ;
+/**
+  Assigns staff to event
+ */
+DELIMITER //
+
+CREATE PROCEDURE assign_to_event(IN role_id_in INT, IN event_id_in INT)
+BEGIN
+    UPDATE role SET event = event_id_in WHERE role_id = role_id_in;
+END //
+
+DELIMITER ;
--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-// @flow
+@flow
 
 const express = require('express');
 const path = require('path');
@@ -31,6 +31,115 @@ const pool = mysql.createPool({
     database: database.database,
     debug: false,
     multipleStatements: true
+});
+
+app.get("/role", (req, res) => {
+    console.log("got get request from client, /role");
+    pool.getConnection((err, connection) => {
+        console.log("connected");
+        if (err) {
+            console.log("error connecting");
+            res.json({error: "error connecting"});
+        } else {
+            console.log("connected2");
+            connection.query(
+                "call get_all_roles()",
+                (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        console.log(err);
+                        res.json({error: "error querying"});
+                    } else {
+                        console.log(rows);
+                        res.json(rows);
+                    }
+                }
+            );
+        }
+    })
+});
+
+app.get("/role/:eventId", (req, res) => {
+    console.log("got get request from client, /role/:eventId");
+    pool.getConnection((err, connection) => {
+        console.log("connected");
+        if (err) {
+            console.log("error connecting");
+            res.json({error: "error connecting"});
+        } else {
+            console.log("connected2");
+            connection.query(
+                "call get_staff_in_event(event_id)",
+                req.params.event,
+                (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        console.log(err);
+                        res.json({error:"error querying"});
+                    } else {
+                        console.log(rows);
+                        res.json(rows);
+                    }
+                }
+            );
+        }
+    })
+});
+
+app.post("/role", (req, res) => {
+    console.log("got post request from client, /role");
+    pool.getConnection((err, connection) => {
+        console.log("connected");
+        if (err) {
+            console.log("error connecting");
+            res.json({error: "error connecting"});
+        } else {
+            console.log("connected2");
+            let val = [req.body.role_id, req.body.type, req.body.event_id];
+            connection.query(
+                "call set_role(role_id, type, event_id",
+                val,
+                (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        console.log(err);
+                        res.json({error: "error querying"});
+                    } else {
+                        console.log(rows);
+                        res.json(rows);
+                    }
+                }
+            );
+        }
+    })
+});
+
+app.put("/role/:roleId", (req, res) => {
+    console.log("got update request from client, /role/:roleId");
+    pool.getConnection((err, connection) => {
+        console.log("connected");
+        if (err) {
+            console.log("error connecting");
+            res.json({error: "error connecting"});
+        } else {
+            console.log("connected2");
+            let val = [req.body.role_id, req.body.event_id];
+            connection.query(
+                "call assign_to_event(role_id, event_id)",
+                val,
+                (err, rows) => {
+                    connection.release();
+                    if (err) {
+                        console.log(err);
+                        res.json({error: "error querying"});
+                    } else {
+                        console.log(rows);
+                        res.json(rows);
+                    }
+                }
+            );
+        }
+    })
 });
 
 // The listen promise can be used to wait for the web server to start (for instance in your tests)
