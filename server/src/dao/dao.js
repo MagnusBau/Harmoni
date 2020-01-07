2f0fe3736c192605500cf65c3adbcc651ebd503a
torstehs <torstehs@stud.ntnu.no>
2020-01-07 13:31:54 +0100

    created base dao file

---
+++ server/src/dao/dao.js
@@ -0,0 +1,29 @@
+// @flow
+module.exports = class Dao {
+    pool: Object;
+    constructor(pool: Object) {
+        this.pool = pool;
+    }
+
+    query(sql: string, params: Array<any>, callback: any) {
+        this.pool.getConnection((err, connection) => {
+            console.log('dao: connected to database');
+            if(err) {
+                console.log('dao: error connecting');
+                callback(500, {error: 'connection error'});
+            } else {
+                console.log('dao: running sql: ' + sql);
+                connection.query(sql, params, (err, rows) => {
+                    connection.release();
+                    if(err) {
+                        console.log(err);
+                        callback(500, {error: 'error querying'});
+                    } else {
+                        console.log('dao: returing rows');
+                        callback(200, rows);
+                    }
+                });
+            }
+        });
+    }
+};
