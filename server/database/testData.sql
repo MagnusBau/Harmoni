b89df2a949eab359338de7e7a4f74e3d0cb34b01
Henrik Mathias Berg <henrimbe@stud.ntnu.no>
2020-01-09 10:47:30 +0100

    server.js updated setup.sql updated testDate.sql updated

--- server/database/testData.sql
+++ server/database/testData.sql
@@ -1,2 +1,2 @@
 INSERT INTO contact(contact_id, first_name, last_name, email, phone) VALUES(default, 'Mario', 'Bros', 'its@me', '01001010');
-INSERT INTO user(user_id, username, password, image, contact) VALUES(DEFAULT, 'Mario', /*password luigi*/'$2a$10$b3uFxV33UaNr5GV6v0M/Au/cFitrqhoKP4iMsKXkcqTUnQ0KnHXAy', '', 01);
+INSERT INTO user(user_id, username, password, image, contact) VALUES(DEFAULT, 'Mario', '$2a$10$58.k1W9JZcldkAaL8RHYx.xBcg7CCFFLUej4BXTxlVYgMHAOQz.2C', '', 01);
