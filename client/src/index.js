6a54fd31bdcc14753b2721e160aec27ff142097e
magnubau <magnubau@stud.ntnu.no>
2020-01-09 14:31:16 +0100

    Made register widget

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route, Redirect } from 'react-router-dom';
 import { AddEquipment} from "./pages/addEquipment";
-import { UserLogin} from "./pages/user";
+import {UserLogin, UserRegister} from "./pages/user";
 
 const root = document.getElementById('app');
 if (root)
@@ -12,6 +12,8 @@ if (root)
         <HashRouter>
             <div>
                 <Route exact path="/login" component={UserLogin} />
+                <Route exact path="/register" component={UserRegister} />
+                <Route exact path="/" component={TokenBoi} />
             </div>
         </HashRouter>,
         root
--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -14,9 +14,10 @@ export class UserLogin extends Component {
     errorMessage: string = "";
 
     attemptLogin() {
-        userService.getLogin(this.username, this.password).then(response => {
+        userService.postLogin(this.username, this.password).then(response => {
             if(response.user != null) {
                 localStorage.setItem("user", response.user);
+                localStorage.setItem("token", response.token);
                 this.errorMessage = "success";
                 history.push("/");
                 return;
@@ -69,11 +70,150 @@ export class UserLogin extends Component {
 
 export class UserRegister extends Component {
 
+    form: any = null;
+    username: string = "";
+    password: string = "";
+    email: string = "";
+    firstname: string = "";
+    lastname: string = "";
+    phone: string = "";
+    errorMessage: string = "";
+
     render() {
         return(
-            <div>
-                yo
+            <div className="row justify-content-center">
+                <div className="card" style={{width: "25%"}}>
+                    <form ref={e => (this.form = e)}>
+                        <p>email:</p>
+                        <input
+                            type="email"
+                            className="form-control"
+                            value={this.email}
+                            placeholder="its@m.e"
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.email = event.target.value)}
+                            required
+                            maxLength={50}
+                        />
+                        <p>Fornavn:</p>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.firstname}
+                            placeholder="Mario"
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.firstname = event.target.value)}
+                            required
+                            maxLength={50}
+                        />
+                        <p>Etternavn:</p>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.lastname}
+                            placeholder="Bros"
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.lastname = event.target.value)}
+                            required
+                            maxLength={50}
+                        />
+                        <p>Telefonnummer:</p>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.email}
+                            placeholder=""
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.email = event.target.value)}
+                            required
+                            minLength={8}
+                            maxLength={12}
+                        />
+                        <p>Brukernavn:</p>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.username}
+                            placeholder="Mario"
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.username = event.target.value)}
+                            required
+                            minLength={1}
+                            maxLength={50}
+                        />
+                        <br/>
+                        <input
+                            type="password"
+                            className="form-control"
+                            value={this.password}
+                            placeholder="Passord"
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.password = event.target.value)}
+                            required
+                            minLength={1}
+                            maxLength={256}
+                        />
+                    </form>
+                    <br/>
+                    <button
+                        type="button"
+                        className="btn btn-dark"
+                        style={{}}
+                        onClick={this.attemptRegister}
+                    >Registrer deg</button>
+                    <br/>
+                    <p style={{color: "red"}}>{this.errorMessage}</p>
+                </div>
             </div>
         )
+    }
+    attemptRegister(){
+        if(!this.form || !this.form.checkValidity()){
+            this.errorMessage = "Fyll ut de røde feltene";
+            this.mounted();
+            return;
+        }
+        let data = {
+            "username": this.username,
+            "password": this.password,
+            "email": this.email,
+            "first_name": this.firstname,
+            "last_name": this.lastname,
+            "phone": this.phone
+        };
+        userService
+            .postRegister(data)
+            .then(response => {
+                if(response.user != null) {
+                    localStorage.setItem("user", response.user);
+                    localStorage.setItem("token", response.token);
+                    this.errorMessage = "success";
+                    history.push("/");
+                    return;
+                }
+                this.errorMessage = "Noe gikk galt";
+            });
+
+    }
+    mounted(){}
+}
+
+export class TokenBoi extends Component{
+    render(){
+        return(
+            <div>
+                <button
+                    type="button"
+                    className="btn btn-dark"
+                    style={{}}
+                    onClick={this.getToken}
+                >Lag ny token</button>
+            </div>
+        )
+    }
+    getToken(){
+        userService
+            .postToken({
+                "user": localStorage.getItem("user"),
+                "token": localStorage.getItem("token")
+            })
+            .then(response => {
+                console.log(localStorage.setItem("token", response.token));
+            });
+
     }
 }
