315d10afe3df20d2864a0b7640ba9a440c4d2f35
victorbl <victorbl@stud.ntnu.no>
2020-01-10 10:23:37 +0100

    test setup

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -42,17 +42,20 @@ CREATE TABLE equipment (
   item VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE event (
-  event_id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(50) NOT NULL,
-  location VARCHAR(100) NOT NULL,
-  start_time DATETIME NOT NULL,
-  end_time DATETIME NOT NULL,
-  category VARCHAR(50),
-  capacity INT NOT NULL,
-  organizer INT NOT NULL,
-  CONSTRAINT event_fk1 FOREIGN KEY(organizer) REFERENCES user(user_id)
+CREATE TABLE event(
+  event_id    INT AUTO_INCREMENT PRIMARY KEY,
+  title       VARCHAR(50)  NOT NULL,
+  description LONGTEXT     NOT NULL,
+  location    VARCHAR(100) NOT NULL,
+  start_time  DATETIME     NOT NULL,
+  end_time    DATETIME     NOT NULL,
+  category    VARCHAR(50),
+  capacity    INT          NOT NULL,
+  organizer   INT          NOT NULL,
+  cancelled   BOOLEAN      NOT NULL DEFAULT FALSE,
+  CONSTRAINT event_fk1 FOREIGN KEY (organizer) REFERENCES user (user_id)
 );
+
 
 CREATE TABLE ticket (
   ticket_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
---
+++ server/tests/dao/eventDao.test.js
@@ -0,0 +1,47 @@
+// @flow
+
+const mysql = require("mysql");
+import {EventDAO} from "../../src/dao/eventDao";
+const runSqlFile = require("../../database/runSqlFile");
+const config = require("../../src/controllers/configuration");
+
+// Create pool for test database
+let database: {} = config.getTestingDatabase();
+let pool = mysql.createPool({
+    connectionLimit: 1,
+    host: database.host,
+    user: database.user,
+    password: database.password,
+    database: database.database,
+    debug: false,
+    multipleStatements: true
+});
+
+const eventDao = new EventDAO(pool);
+
+beforeAll(done => {
+    runSqlFile("database/setup.sql",
+        pool, () => {
+            runSqlFile("database/procedures/event_procedures.sql", pool, () => {
+                runSqlFile("database/testData.sql", pool, done);
+            })
+        });
+});
+
+afterAll(() => {
+    pool.end();
+});
+
+test("Get all events", done => {
+   function callback(status, data) {
+       console.log(
+           `Test callback: status=${status}, data=${data}`
+       );
+       data = data[0];
+       expect(data.length).toBe(2);
+       expect(data[0].title).toBe("EM Håndball");
+       expect(data[1].title).toBe("Konsert");
+   }
+   eventDao.getAllEvent(callback);
+});
+
