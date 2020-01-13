bb2acbe7c80195dac061a7b5bcbb3ba2b4d1075f
Cato Bakken <cato.bakken@ntnu.no>
2020-01-13 09:09:19 +0100

    .

--- client/src/pages/roleStuff.js
+++ client/src/pages/roleStuff.js
@@ -11,6 +11,10 @@ export class AddRole extends Component <{match: {params: {id: number}}}> {
     event: number = 1;
     roles: Role[] = [];
     newRole: Role = null;
+
+    constructor(props, context) {
+        super(props, context);
+    }
     mounted() {
         roleService
             .getAllRoles()
@@ -18,12 +22,18 @@ export class AddRole extends Component <{match: {params: {id: number}}}> {
             .catch((error: Error) => console.log(error.message));
     }
     onChange(e) {
+        const type = e.target.name;
+        this.newRole[type] = e.target.name;
 
     }
     onSubmit(e) {
         e.preventDefault();
-        roleService.createRole(this.newRole.type, this.event);
+        roleService.createRole({type: this.newRole.type, event: this.event});
         this.newRole = null;
+        window.location.reload();
+    }
+    remove(role){
+        roleService.removeRol({roleId: role.role_id});
         window.location.reload();
     }
     render() {
@@ -31,20 +41,21 @@ export class AddRole extends Component <{match: {params: {id: number}}}> {
             <div className="m-2">
                 <form className={"form-inline"} onSubmit={this.onSubmit}>
                     <div className="form-group m-2">
-                        <input type="text" className="form-control" placeholder="Rollenavn"/>
+                        <input type="text"
+                               className="form-control"
+                               id="role-type"
+                               placeholder="Rollenavn"
+                               onChange={this.onChange}/>
                     </div>
                     <button type="submit" className="btn-primary m-2">Legg til</button>
                 </form>
                 <table className="table w-50">
-                    <thead>
-                    <tr>
-                        <th>Personell</th>
-                    </tr>
-                    </thead>
+                    <thead><tr><th>Personell</th></tr></thead>
                     <tbody>
                     {this.roles.map((role =>
                             <tr>
                                 <td>{role.type}</td>
+                                <button className="btn-danger" onClick={this.remove}>Fjern</button>
                             </tr>
                     ))}
                     </tbody>
--- client/src/services/roleService.js
+++ client/src/services/roleService.js
@@ -15,16 +15,16 @@ class RoleService {
         return axios.get<Role[]>('http://localhost:4000/role?event=$' + eventId).then(response => response.data);
     }
     createRole(role: Role) {
-        return axios.post<Role, void>('http://localhost:4000/role', role).then(response => response.data);
+        return axios.post<Role, void>('http://localhost:4000/role', {type: role.type, event: role.event});
     }
     assignRole(role: Role) {
-        return axios.put<Role, void>('http://localhost:4000/role', role).then(response => response.data);
+        return axios.put<Role, void>('http://localhost:4000/role', {type: role.type, event: role.event});
     }
     removeRoleFromEvent(role: Role) {
-        return axios.put<Role, void>('http://localhost:4000/role', role).then(response => response.data);
+        return axios.put<Role, void>('http://localhost:4000/role', {type: role.type, event: role.event});
     }
     removeRol(roleId: number) {
-        return axios.delete<void>('http://localhost:4000/role', roleId).then(response => response.data);
+        return axios.delete<void>('http://localhost:4000/role', {role_id: roleId});
     }
 }
 
