bda2202a9285504b5dc19c2ce8a2cbd6e4aec024
magnubau <magnubau@stud.ntnu.no>
2020-01-13 12:15:07 +0100

    trying to fix riderDao tests

--- server/database/testData.sql
+++ server/database/testData.sql
@@ -36,10 +36,10 @@ VALUES (1, 4, 4);
 INSERT INTO event_equipment (event, equipment, amount)
 VALUES (2, 4, 8);
 
-INSERT INTO document (name, file, event)
-VALUES ('thrud', 'thrud', 1);
-INSERT INTO document (name, file, event)
-VALUES ('faor', 'faor', 1);
+INSERT INTO document (document_id, name, file, event)
+VALUES (DEFAULT, 'thrud', 'thrud', 1);
+INSERT INTO document (document_id, name, file, event)
+VALUES (DEFAULT, 'faor', 'faor', 1);
 
 INSERT INTO rider (description, document)
 VALUES ('Mathias må ha tre kameler og syv geiter', 2);
