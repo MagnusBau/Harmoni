c75b0906df59ec220d72211ef4e1fc099f4a615e
victorbl <victorbl@stud.ntnu.no>
2020-01-15 16:09:52 +0100

    kan se de forskjellig tickets for en event, men ikke redigere

--- server/src/routes/ticket.js
+++ server/src/routes/ticket.js
@@ -1,16 +1,10 @@
 //@flow
-
 const express = require('express');
-
 const ticketController = require('../controllers/ticket');
-
 const router = express.Router();
-
-router.get("/all/:event", ticketController.getAllTickets);
-router.get("/", ticketController.getAllTickets);
-router.get("/:ticketId", ticketController.getTicketById);
-router.post("/", ticketController.insertTicket);
-router.put("/:ticketId", ticketController.updateTicket);
-router.delete("/:ticketId", ticketController.deleteTicket);
-
+router.get("/event/:event", ticketController.getAllTickets);
+router.get("/ticket/:ticketId", ticketController.getTicketById);
+router.post("/ticket", ticketController.insertTicket);
+router.put("/ticket/:ticketId", ticketController.updateTicket);
+router.delete("/ticket/:ticketId", ticketController.deleteTicket);
 module.exports = router;
