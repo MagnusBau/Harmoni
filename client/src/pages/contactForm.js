7b189f04b2c998cc4332855f86957d7689beeb95
Mia Fornes <miafo@stud.ntnu.no>
2020-01-20 13:48:54 +0100

    service and updated frontendfor contact form

--- client/src/pages/contactForm.js
+++ client/src/pages/contactForm.js
@@ -3,12 +3,14 @@
 import * as React from "react";
 import {Component} from "react-simplified";
 import {createHashHistory} from "history";
-import {Column} from "../components/widgets";
-import {Button} from "../components/widgets";
+import { contactUsService, ContactUs } from '../services/contactService';
 
 const history = createHashHistory();
 
 export class ContactForm extends Component {
+
+    contactUsData = new ContactUs();
+    form = null;
 
     render() {
 
@@ -18,64 +20,79 @@ export class ContactForm extends Component {
 
                 <div style={{textAlign: 'center'}}>
                     <h1>Kontakt oss</h1>
-                    <p>Bruk dette skjemaet for spørsmål, idéer, tilbakemeldinger eller andre henvendelser. </p>
+                    <p style={{marginTop: 15}}>Bruk dette skjemaet for spørsmål, idéer, tilbakemeldinger eller andre henvendelser. </p>
                 </div>
 
-                <form className="form-group">
+                <form ref={e => {this.form = e}} className="form-group">
 
                     <div className={"form-group m-2"}>
-                        <label>Navn</label>
+                        <label>Navn*</label>
                         <p><input
                             className="form-control"
                             type="text"
                             placeholder="Skriv inn fullt navn"
-                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => this.heading = event.target.value}
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => this.contactUsData.name = event.target.value}
+                            required={true}
                         /></p>
                     </div>
 
                     <div className={"form-group m-2"}>
-                        <label>Epost</label>
+                        <label>Epost*</label>
                         <p><input
                             className="form-control"
                             type="text"
                             placeholder="Skriv inn din epostadresse"
-                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => this.heading = event.target.value}
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => this.contactUsData.email = event.target.value}
+                            required={true}
                         /></p>
                     </div>
 
-                    <h4 style={{marginBottom: 12}}>Din henvendelse</h4>
+                    <h4 style={{marginBottom: 12, marginLeft: 8, fontSize: 21}}>Din henvendelse</h4>
 
                     <div className={"form-group m-2"}>
-                        <label>Hva gjelder det?</label>
+                        <label>Hva gjelder det?*</label>
                         <p>
-                            <select className="form-control" required>
+                            <select className="form-control" onChange={(event: SyntheticInputEvent<HTMLInputElement>) => this.contactUsData.subject = event.target.value} required={true}>
                                 <option value="" hidden>Velg emne </option>
-                                <option value={1}>Spørsmål</option>
-                                <option value={2}>Idéer</option>
-                                <option value={3}>Melding om feil/bugs</option>
+                                <option value={"Spørsmål"}>Spørsmål</option>
+                                <option value={"Idéer"}>Idéer</option>
+                                <option value={"Melding om feil/bugs"}>Melding om feil/bugs</option>
                         </select></p>
                     </div>
 
                     <div className={"form-group m-2"}>
-                        <label>Melding</label>
+                        <label>Melding*</label>
                         <p><textarea
                             className="form-control"
                             style={{height: 200}}
                             placeholder="Skriv din melding"
-                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => this.heading = event.target.value}
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => this.contactUsData.content = event.target.value}
+                            required={true}
                         /></p>
                     </div>
 
-                    <Column width={3}>
-                        <Button.Blue onClick={this.sendEmail}>Send</Button.Blue>
-                    </Column>
-
                 </form>
+
+                <div style={{textAlign: "center"}}>
+                    <button type="button" className="btn btn-outline-primary" onClick={this.sendEmail}>Send</button>
+                </div>
+
             </div>
         )
     }
 
     sendEmail() {
+
+        if (!this.form || !this.form.checkValidity())  {
+            return (
+                Alert.danger("Vennligst fyll ut alle feltene")
+            );
+        }
+
+        contactUsService
+            .contactUs(this.contactUsData)
+            .then(window.location.reload())
+            .catch((error: Error) => console.log(error))
 
     }
 
