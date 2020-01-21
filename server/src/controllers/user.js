c82a2b844ced1d6b74d9dd5230629ca48fdca9b3
Asbjørn Fiksdal Kallestad <asbjorfk@stud.ntnu.no>
2020-01-21 12:40:23 +0100

    some fix

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -183,7 +183,8 @@ function validateFirstName(data: Object, first_name: string, last_name: string, 
 }
 
 function validateLastName(data: Object, last_name: string, phone: string, res: Response) {
-    if(last_name.match("^[A-Za-z-æøåÆØÅ]+$") && 2 < last_name.length < 50) {
+    let regex =/^[A-Za-z-æøåÆØÅ]{3,40}$/;
+    if(last_name.match(regex) && 2 < last_name.length < 50) {
         return validatePhone(data, phone, res);
     } else {
         console.log("Invalid last name");
@@ -194,7 +195,7 @@ function validateLastName(data: Object, last_name: string, phone: string, res: R
 
 function validatePhone(data: Object, phone: string, res: Response) {
     if(!phone.match(/\D/)) {
-        if(phone.length == 8) {
+        if(phone.length === 8) {
             return register(data, res);
         } else if(phone.length === 12 && phone.substring(0, 3) === "0047") {
             return register(data, res);
@@ -305,8 +306,9 @@ exports.updateUser = (req, res, next) => {
     let data: Object = req.body;
     let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(re.test(String(data.email).toLowerCase())) {
-        if(data.first_name.match("^[A-Za-z-æøåÆØÅ]+$") && 2 < data.first_name.length < 50) {
-            if(data.last_name.match("^[A-Za-z-æøåÆØÅ]+$") && 2 < data.last_name.length < 50) {
+        let regexName =/^[A-Za-z-æøåÆØÅ]{3,40}$/;
+        if(data.first_name.match(regexName) && 2 < data.first_name.length < 50) {
+            if(data.last_name.match(regexName) && 2 < data.last_name.length < 50) {
                 if(data.phone.length === 8 || (data.phone.length === 12 && data.phone.substring(0, 3) === "0047")) {
                     userDao.getContact(id, (err, rows) => {
                         if(rows[0][0].contact_id) {
@@ -408,7 +410,8 @@ exports.updateUserPassword = (req, res, next) => {
     let password = req.body.password;
     let newPassword = req.body.newPassword;
     let id = req.params.userId;
-    if(password.match("^[A-Za-z0-9-æøåÆØÅ]+$") && 2 < password.length <= 256) {
+    let regex =/^[A-Za-z0-9-æøåÆØÅ]{8,256}$/;
+    if(password.match(regex)){
         userDao.getPassword(req.body.username, (err, rows) => {
             if(rows[0][0]) {
                 if(rows[0][0].password) {
