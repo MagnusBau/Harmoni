bd835d0a5cee2ded1c52070dcedaa533eb24b9da
victorbl <victorbl@stud.ntnu.no>
2020-01-21 12:15:13 +0100

    minor fix in get_all_events() procedure

--- server/database/procedures/event_procedures.sql
+++ server/database/procedures/event_procedures.sql
@@ -51,7 +51,7 @@ BEGIN
   SELECT event_id,
          title,
          location,
-         DATE_FORMAT(start_time, "%a %e.%m.%Y %H:%i") as start_time,
+         DATE_FORMAT(start_time, '%a %e.%m.%Y %H:%i') as start_time,
          category
   FROM event WHERE cancelled = 0 ORDER BY start_time LIMIT 9;
 END;
