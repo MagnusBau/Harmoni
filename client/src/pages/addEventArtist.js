a386c076777605b179836cef40d5c7bbcb4fea10
magnubau <magnubau@stud.ntnu.no>
2020-01-22 08:36:44 +0100

    Fixed same issue with contracts

--- client/src/pages/addEventArtist.js
+++ client/src/pages/addEventArtist.js
@@ -124,6 +124,11 @@ export class AddEventArtist extends Component {
         let file = this.state.file;
         let formData = new FormData();
         e.preventDefault();
+        if(this.name !== file.name){
+            if(this.name.slice((Math.max(0, this.name.lastIndexOf(".")) || Infinity) + 1) !== file.name.slice((Math.max(0, file.name.lastIndexOf(".")) || Infinity) + 1)){
+                this.name = this.name + "." + file.name.slice((Math.max(0, file.name.lastIndexOf(".")) || Infinity) + 1)
+            }
+        }
         if(Number.parseInt(this.documentId) === -1){
             if(this.state.file !== null){
                 fileInfoService.checkFileName(this.props.eventId, this.name)
@@ -131,7 +136,7 @@ export class AddEventArtist extends Component {
                         console.log("DUP?: "+ response[0][0].duplicate);
                         if(response[0][0].duplicate === 0){
 
-                            const myNewFile = new File([file], this.props.eventId + this.nameAddOn + file.name, {type: file.type});
+                            const myNewFile = new File([file], this.props.eventId + this.nameAddOn + this.name, {type: file.type});
 
                             formData.append('file', myNewFile);
                             formData.append('name', this.name);
