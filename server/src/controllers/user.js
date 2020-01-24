6931b35c1d5a39963d5b7872ab4513a991bbb28b
Asbjørn Fiksdal Kallestad <asbjorfk@stud.ntnu.no>
2020-01-24 23:13:36 +0100

    user comments

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -53,6 +53,15 @@ const signOptions = {
     algorithm:  "RS256"
 };
 
+/**
+ *Is the function for login. Check if the different user input is valid. If valid user gets logged in if not it
+ * return an error
+ * @param bool return the information regarding the login.
+ * @param username is te username of the user
+ * @param res response to service
+ */
+
+
 function login(bool: boolean, username: string, res: Response) {
     if (bool) {
         console.log(TAG, "Brukernavn & passord ok");
@@ -124,6 +133,22 @@ function login(bool: boolean, username: string, res: Response) {
     }
 }
 
+
+/**
+ * This methode takes in all user information check if user name is valid.(canot be equal to another allready in the database) and if it is it sends
+ * you to the validation methode for passwords
+ * norwegian letters is allowed
+ * @param data
+ * @param username
+ * @param password
+ * @param email
+ * @param first_name
+ * @param last_name
+ * @param phone
+ * @param res sends result to service
+ * @returns {boolean}
+ */
+
 function validateUsername(data: Object, username: string, password: string, email: string, first_name: string, last_name: string, phone: string, res: Response) {
     let regex =/^[A-Za-z0-9-æøåÆØÅ]{4,30}$/;
     if(username.match(regex)) {
@@ -153,6 +178,18 @@ function validateUsername(data: Object, username: string, password: string, emai
     }
 }
 
+/**
+ * Validate the user password, if it is, it sends you to emailvalidation
+ * @param data
+ * @param password
+ * @param email
+ * @param first_name
+ * @param last_name
+ * @param phone
+ * @param res
+ * @returns {void|boolean|undefined}
+ */
+
 function validatePassword(data: Object, password: string, email: string, first_name: string, last_name: string, phone: string, res: Response) {
     let regex =/^[A-Za-z0-9-æøåÆØÅ]{5,256}$/;
     if(password.match(regex)){
@@ -163,6 +200,17 @@ function validatePassword(data: Object, password: string, email: string, first_n
         return false;
     }
 }
+
+/**
+ * Validate the users email, with different requirements. Sends you to validate first name if suqsess.
+ * @param data
+ * @param email
+ * @param first_name
+ * @param last_name
+ * @param phone
+ * @param res
+ * @returns {void|boolean}
+ */
 
 function validateEmail(data: Object, email: string, first_name: string, last_name: string, phone: string, res: Response) {
     if(email.length < 50) {
@@ -177,6 +225,18 @@ function validateEmail(data: Object, email: string, first_name: string, last_nam
     }
 }
 
+/**
+ * Validate first name.  can be between 3 to 40 characters.
+ * norwegian characters are allowed
+ * if true send to validate last
+ * @param data reacive all the user information
+ * @param first_name
+ * @param last_name
+ * @param phone
+ * @param res
+ * @returns {void|boolean}
+ */
+
 function validateFirstName(data: Object, first_name: string, last_name: string, phone: string, res: Response) {
     let regex =/^[A-Za-z-æøåÆØÅ]{3,40}$/;
     if(first_name.match(regex)) {
@@ -188,6 +248,17 @@ function validateFirstName(data: Object, first_name: string, last_name: string, 
     }
 }
 
+/**validate last name. Cant be between 3 to 40 characters
+ * Norwegian chars are allowed
+ * if succsessd returns validate phone
+ *
+ * @param data all user data
+ * @param last_name
+ * @param phone
+ * @param res response to server
+ * @returns {void|boolean}
+ */
+
 function validateLastName(data: Object, last_name: string, phone: string, res: Response) {
     let regex =/^[A-Za-z-æøåÆØÅ]{3,40}$/;
     if(last_name.match(regex)) {
@@ -198,6 +269,16 @@ function validateLastName(data: Object, last_name: string, phone: string, res: R
         return false;
     }
 }
+
+/**
+ * validate phone.
+ * must be digeds and have a sertain lenght 8 or 12
+ * if true return all data to register methode
+ * @param data
+ * @param phone
+ * @param res response to server
+ * @returns {boolean|void}
+ */
 
 function validatePhone(data: Object, phone: string, res: Response) {
     if(!phone.match(/\D/)) {
@@ -216,6 +297,12 @@ function validatePhone(data: Object, phone: string, res: Response) {
         return false;
     }
 }
+
+/**
+ * register user check. incrupte user password and registrer user if valid.
+ * @param data all user data
+ * @param res
+ */
 
 function register(data: Object, res: Response) {
     // Store original password to send by mail
@@ -249,9 +336,14 @@ function register(data: Object, res: Response) {
     });
 }
 
-// Håndterer login og sender JWT-token tilbake som JSON
+/**
+ * Håndterer login og sender JWT-token tilbake som JSON
+ * om bruker har et savedhash methode return to user log in.
+ */
+
+
 exports.loginUser = (req, res, next) => {
-    console.log(TAG, "yo");
+
     userDao.getPassword(req.body.username, (err, rows) => {
         let savedHash = null;
         if(rows[0]) {
@@ -270,14 +362,22 @@ exports.loginUser = (req, res, next) => {
     });
 };
 
+/**
+ * returnes artist that is linked to an user(host)
+ */
+
 exports.getUserByArtist = (req, res, next) => {
-    console.log(TAG, `Got request from client: GET /auth/user/artist/${req.params.artistId}`);
+
 
     userDao.getUserByArtist(req.params.artistId, (err, rows) => {
 
         res.send(rows);
     });
 };
+
+/**
+ * this calls the chain of validation and if all passes it successfully registres the user. if not it returnes an error of what went bad
+ */
 
 exports.registerUser = (req, res, next) => {
     let data = {
@@ -292,9 +392,13 @@ exports.registerUser = (req, res, next) => {
         "organizer": req.body.organizer
     };
     if(validateUsername(data, req.body.username, req.body.password, req.body.email, req.body.first_name, req.body.last_name, req.body.phone, res)) {
-        console.log(TAG, "yo (bad)");
+
     }
 };
+
+/**
+ * gets an user token
+ */
 
 exports.getToken = (req, res, next) => {
     console.log(TAG, "Skal returnere en ny token");
@@ -305,7 +409,13 @@ exports.getToken = (req, res, next) => {
 };
 
 
-
+/**
+ * Updates an user.
+ * gets in user data, and makes it possible to change.
+ * @param req
+ * @param res
+ * @param next func to run
+ */
 exports.updateUser = (req, res, next) => {
     console.log(TAG, "Skal oppdatere bruker");
     console.log(TAG, req.body);
@@ -348,6 +458,13 @@ exports.updateUser = (req, res, next) => {
 
 };
 
+
+/**
+ * returne an user with its data on user id
+ * @param req requst to service
+ * @param res responce to service
+ * @param next nexct func
+ */
 exports.getUser = (req, res, next) => {
     console.log(TAG, "id:" + req.params.userId);
     userDao.getUserById(req.params.userId, (err, user) => {
@@ -412,6 +529,10 @@ exports.getUser = (req, res, next) => {
         });
     });
 };
+
+/**
+ * updates the users password.
+ */
 
 exports.updateUserPassword = (req, res, next) => {
     let password = req.body.password;
