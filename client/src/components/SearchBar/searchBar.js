1e1f9076086d658ce0495bc61e3ee5e9a291d47d
Cato Bakken <cato.bakken@ntnu.no>
2020-01-24 11:50:46 +0100

    fixed some shizz

--- client/src/components/SearchBar/searchBar.js
+++ client/src/components/SearchBar/searchBar.js
@@ -15,7 +15,6 @@ const history = createHashHistory();
 //TODO legge til egen søk side, slik at når trykker enter så kommer siden opp
 export class SearchBar extends Component {
     events: EventSearch[] = [];
-
     input: string = "";
 
     render() {
@@ -33,7 +32,7 @@ export class SearchBar extends Component {
                         ))}
                     </div>
                 </div>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="button" onClick={() => history.push("/event/search/" + this.input)}>Søk</button>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="button" onClick={() => this.buttonSearch()}>Søk</button>
             </div>
         );
     }
@@ -54,26 +53,20 @@ export class SearchBar extends Component {
         history.push("/event/" + e + "/view")
     }
     ifEnter = (event) => {
+        if(this.input !== '')
         if(event.key === 'Enter'){
             console.log("enter");
             history.push("/event/search/" + this.input);
+            this.input = '';
         }
     };
-
+    buttonSearch() {
+        if(this.input !== '') history.push("/event/search/" + this.input);
+    }
     ifEnterInList = (event) => {
         if(event.key === 'Enter'){
             console.log("enter");
             history.push("/event/" + event.event_id + "/view");
         }
-    };
-
-    view = (event) => {
-        let id = event.target.getAttribute('event_id');
-        history.push("/event/" + id)
-    };
-
-    search = (event) => {
-        this.input = event.target.getAttribute('author');
-        this.mounted();
     };
 }
