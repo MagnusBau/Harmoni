f68fb9a379488f2d00c0ec12aa5d764b77f8d83d
Cato Bakken <cato.bakken@ntnu.no>
2020-01-24 21:33:12 +0100

    yoyoyo

--- server/tests/dao/userDao.test.js
+++ server/tests/dao/userDao.test.js
@@ -130,3 +130,62 @@ test('get organizer username', done =>  {
     }
     userDao.getOrganizerUsername(1, callback)
 });
+
+test('get user by id', done => {
+    function callback(status, data) {
+        console.log(
+            `Test callback: status=${status}, data=${data}`
+        );
+        data = data[0];
+        expect(data[0].username).toBe('Mario');
+        done();
+    }
+    userDao.getUserById(1, callback);
+});
+
+test('get contact by id', done => {
+    function callback(status, data) {
+        console.log(
+            `Test callback: status=${status}, data=${data}`
+        );
+        data = data[0];
+        expect(data[0].first_name).toBe('Mario');
+        expect(data[0].last_name).toBe('Bros');
+        done();
+    }
+    userDao.getContact(1, callback)
+});
+
+test('get user by artist', done => {
+    function callback(status, data) {
+        console.log(
+            `Test callback: status=${status}, data=${data}`
+        );
+        data = data[0];
+        expect(data[0].username).toBe('miafornes');
+        done();
+    }
+    userDao.getUserByArtist(2, callback);
+});
+
+test('update contact', done => {
+    function callback(status, data) {
+        console.log(
+            `Test callback: status=${status}, data=${data}`
+        );
+        expect(data.affectedRows).toBe(1);
+        done();
+    }
+    userDao.updateContact(1, {first_name: 'Joe', last_name: 'Mama', email: 'joe@mama.com', phone: 69696969}, callback);
+});
+
+test('update password', done => {
+    function callback(status, data) {
+        console.log(
+            `Test callback: status=${status}, data=${data}`
+        );
+        expect(data.affectedRows).toBe(1);
+        done();
+    }
+    userDao.updatePassword(1, 'luigi', callback);
+});
